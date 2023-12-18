f53e0199773e6eb3c8b55c3e638151129707cf1a
Rasel <ruselcucho@gmail.com>
2023-12-18 01:21:42 -0500

    Creación de las clases de asignar encargado

---
+++ API.Facultad/Api.Facultad.Application/Features/Facultad/Command/AsignarEncargado/AsignarEncargadoCommand.cs
@@ -0,0 +1,12 @@
+﻿using Api.Facultad.Domain.DTOs.Base;
+using MediatR;
+
+namespace Api.Facultad.Application.Features.Facultad.Command.AsignarEncargado
+{
+    public class AsignarEncargadoCommand : RequestBase, IRequest<ResponseBase>
+    {
+        public string IdEncargado { get; set; } = string.Empty;
+        public string IdFacultad { get; set; } = string.Empty;
+
+    }
+}
---
+++ API.Facultad/Api.Facultad.Application/Features/Facultad/Command/AsignarEncargado/AsignarEncargadoCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using Api.Facultad.Application.Contracts.Persistence;
+using Api.Facultad.Application.Features.Facultad.Queries.GetFacultadById;
+using Api.Facultad.Application.Utils;
+using Api.Facultad.Domain.DTOs.Base;
+using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Facultad.Application.Features.Facultad.Command.AsignarEncargado
+{
+    public class AsignarEncargadoCommandHandler : IRequestHandler<AsignarEncargadoCommand, ResponseBase>
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly IMapper _mapper;
+        private readonly ILogger<AsignarEncargadoCommandHandler> _logger;
+        public AsignarEncargadoCommandHandler(IUnitOfWork uow, IMapper mapper, ILogger<AsignarEncargadoCommandHandler> logger)
+        {
+            _uow = uow;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task<ResponseBase> Handle(AsignarEncargadoCommand request, CancellationToken cancellationToken)
+        {
+            try
+            { 
+                              
+            
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "", ex.Message);
+                return ResponseUtil.InternalError();
+            }
+        }
+    }
+}
