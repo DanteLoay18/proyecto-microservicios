cca2b1bbc01c8df5989ad9f579a467a42e436732
Dante Loayza <danteloayza8@gmail.com>
2023-12-19 23:51:38 -0500

    se agrego el validator para asignar y eliminar encargado

---
+++ API.Facultad/Api.Facultad.Application/Features/Facultad/Command/AsignarEncargado/AsignarEncargadoCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿
+using FluentValidation;
+
+namespace Api.Facultad.Application.Features.Facultad.Command.AsignarEncargado
+{
+    public class AsignarEncargadoCommandValidator : AbstractValidator<AsignarEncargadoCommand>
+    {
+
+        public AsignarEncargadoCommandValidator()
+        {
+            RuleFor(x => x.IdFacultad)
+            .NotEmpty()
+            .WithMessage("El campo IdFacultad es obligatorio.");
+
+            RuleFor(x => x.IdUsuario)
+            .NotEmpty()
+            .WithMessage("El campo IdUsuario es obligatorio.");
+
+            RuleFor(x => x.IdEncargado)
+               .NotEmpty()
+               .WithMessage("El campo IdUsuario es obligatorio.");
+        }
+    }
+}
---
+++ API.Facultad/Api.Facultad.Application/Features/Facultad/Command/DeleteEncargado/DeleteEncargadoCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿
+using FluentValidation;
+
+namespace Api.Facultad.Application.Features.Facultad.Command.DeleteEncargado
+{
+    public class DeleteEncargadoCommandValidator : AbstractValidator<DeleteEncargadoCommand>
+    {
+        public DeleteEncargadoCommandValidator()
+        {
+            RuleFor(x => x.IdFacultad)
+            .NotEmpty()
+            .WithMessage("El campo IdFacultad es obligatorio.");
+
+            RuleFor(x => x.IdUsuario)
+            .NotEmpty()
+            .WithMessage("El campo IdUsuario es obligatorio.");
+        }
+    }
+}
