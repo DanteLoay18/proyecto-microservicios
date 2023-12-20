09c142ef695c6c555007850eb16506bc459212fe
Dante Loayza <danteloayza8@gmail.com>
2023-12-19 22:58:07 -0500

    Se creo unas constantes para auditoria

--- API.Facultad/Api.Facultad.Application/Mappers/FacultadProfile.cs
+++ API.Facultad/Api.Facultad.Application/Mappers/FacultadProfile.cs
@@ -1,6 +1,7 @@
 ﻿
 using Api.Facultad.Application.Features.Facultad.Command.AsignarEncargado;
 using Api.Facultad.Application.Features.Facultad.Command.DeleteEncargado;
+using Api.Facultad.Domain.Constants.Base;
 using Api.Facultad.Domain.DTOs.Response;
 using Api.Facultad.Domain.Entities;
 using AutoMapper;
@@ -24,13 +25,14 @@ namespace Api.Facultad.Application.Mappers
                 .ForMember(d => d.Encargado, opt => opt.MapFrom(src => src.IdEncargado))
                 .ForMember(d => d.ModifiedBy, opt => opt.MapFrom(src => src.IdUsuario))//quien  hace petición
                 .ForMember(d => d.ModifiedAt, opt => opt.MapFrom(src => DateTime.Now))//momento de modificación
-                .ForMember(d => d.ModifiedFrom, opt => opt.MapFrom(src => "Asignar encargado"));//tipo de modificacion
+                .ForMember(d => d.ModifiedFrom, opt => opt.MapFrom(src => AuditoriaConstant.FromAsignarEncargado));//tipo de modificacion
 
             CreateMap<DeleteEncargadoCommand, Facultade>()
                .ForMember(d => d.ModifiedBy, opt => opt.MapFrom(src => src.IdUsuario))
                .ForMember(d => d.ModifiedAt, opt => opt.MapFrom(src => DateTime.Now))
-               .ForMember(d => d.ModifiedFrom, opt => opt.MapFrom(src => "Eliminar encargado"))
+               .ForMember(d => d.ModifiedFrom, opt => opt.MapFrom(src => AuditoriaConstant.FromDeleteEncargado))
                .AfterMap((src, dest) => dest.Encargado = null);
+
         }
 
     }
---
+++ API.Facultad/Api.Facultad.Domain/Constants/Base/AuditoriaConstant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Facultad.Domain.Constants.Base
+{
+    public static class AuditoriaConstant
+    {
+        public const string FromAsignarEncargado = "Asignar Encargado";
+        public const string FromDeleteEncargado = "Delete Encargado";
+    }
+}
