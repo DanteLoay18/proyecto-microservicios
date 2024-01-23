c7da1e9b5ebea777a4891de7362cbf6c5c9de1ce
RenzoCS99 <67722440+RenzoCS99@users.noreply.github.com>
2024-01-22 20:09:40 -0500

    Gateway Service

--- API.Presentacion/src/Expediente/expediente.service.ts
+++ API.Presentacion/src/Expediente/expediente.service.ts
@@ -1,6 +1,5 @@
 import { Injectable,Inject, BadRequestException } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
-
 import { map, tap, lastValueFrom, mergeMap, } from 'rxjs';
 import { UsuarioService } from 'src/Auth/usuario.service';
 import { FacultadService } from 'src/Facultad/facultad.service';
@@ -22,7 +21,12 @@ export class ExpedienteService {
     ) {}
   
   async findAllPaginated(page:number, pageSize:number, rolUsuario:string, idUsuario:string){
-   
+    try {
+      const resp = await axios.get(`${this.rootInternet}${this.apiExpediente}${this.rootApi}${this.nombreExpediente}?Page=${page}&PageSize=${pageSize}`)
+      return resp.data;
+    } catch (error) {
+      return error;
+    }
   }
 
  
@@ -47,26 +51,88 @@ export class ExpedienteService {
   }
 
   async findByBusqueda(findByBusquedaPaginatedRequest:FindByBusquedaPaginatedRequest,rolUsuario:string, idUsuario:string){
+    findByBusquedaPaginatedRequest: FindByBusquedaPaginatedRequest,
+    rolUsuario: string,
+    idUsuario: string
+  ) {
+    try {
+      const resp = await this.clienteUser.send({ cmd: 'find_by_busqueda' }, { request: findByBusquedaPaginatedRequest, rolUsuario, idUsuario }).pipe(
+        tap(({ success, message }) => { if (!success) throw new BadRequestException(message) }),
+        map(({ value, ...rest }) => ({
+          ...rest,
+          value
+        })),
+      ).toPromise();
 
-   
+      return resp.value;
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
   }
 
   async createExpediente(createExpedienteRequest:CreateExpedienteRequest,rolUsuario:string, idUsuario:string){
+    try {
+      const resp = await this.clienteUser.send({ cmd: 'create_expediente' }, { request: createExpedienteRequest, rolUsuario, idUsuario }).pipe(
+        tap(({ success, message }) => { if (!success) throw new BadRequestException(message) }),
+        map(({ value, ...rest }) => ({
+          ...rest,
+          value
+        })),
+      ).toPromise();
 
+      return resp.value;
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
   }
 
   async updateExpediente(updateExpedienteRequest:UpdateExpedienteRequest,rolUsuario:string, idUsuario:string){
+    try {
+      const resp = await this.clienteUser.send({ cmd: 'update_expediente' }, { request: updateExpedienteRequest, rolUsuario, idUsuario }).pipe(
+        tap(({ success, message }) => { if (!success) throw new BadRequestException(message) }),
+        map(({ value, ...rest }) => ({
+          ...rest,
+          value
+        })),
+      ).toPromise();
 
-   
+      return resp.value;
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
   }
 
-  async validarExpediente(validarExpedienteRequest:ValidarExpedienteRequest, idUsuario:string){
-  
-    
+  async validarExpediente(validarExpedienteRequest:ValidarExpedienteRequest, idUsuario:string){ 
+    try {
+      const resp = await this.clienteUser.send({ cmd: 'validar_expediente' }, { request: validarExpedienteRequest, idUsuario }).pipe(
+        tap(({ success, message }) => { if (!success) throw new BadRequestException(message) }),
+        map(({ value, ...rest }) => ({
+          ...rest,
+          value
+        })),
+      ).toPromise();
+
+      return resp.value;
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }    
   }
 
   async eliminarExpediente(idExpediente:string,rolUsuario:string, idUsuario:string){
-   
+    try {
+      const resp = await this.clienteUser.send({ cmd: 'eliminar_expediente' }, { idExpediente, rolUsuario, idUsuario }).pipe(
+        tap(({ success, message }) => { if (!success) throw new BadRequestException(message) }),
+        map(({ value, ...rest }) => ({
+          ...rest,
+          value
+        })),
+      ).toPromise();
+
+      return resp.value;
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
   }
  
     
